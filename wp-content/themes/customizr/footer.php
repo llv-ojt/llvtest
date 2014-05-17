ae6cf81322cc2d53d6ef3ba68b76d6b6d517cedc
rj <mendozarj>
2014-05-17 22:20:00 +0800

    Footer.php

--- wp-content/themes/customizr/footer.php
+++ wp-content/themes/customizr/footer.php
@@ -10,8 +10,29 @@ do_action( '__before_footer' );
 ?>
 	<!-- FOOTER -->
 	<footer id="footer" class="<?php echo tc__f('tc_footer_classes', '') ?>">
-            <span id="s_menu" class="s_menu"><?php $name = array('uno' => 'Rj Mendoza'); print($name['uno']);?></span>
-                <?php do_action( '__footer' ); // hook of footer widget and colophon?>
+            <div id="s_menu" class="s_menu">
+                <ul>
+                    <li>
+                        HOME
+                    </li>
+                    <li>
+                        ROOMS
+                    </li>
+                    <li>
+                        RESORT
+                    </li>
+                    <li>
+                        ATTRACTIONS
+                    </li>
+                    <li>
+                        ABOUT
+                    </li>
+                    <li>
+                        CONTACT
+                    </li>                    
+                </ul>
+            </div>        
+            <?php do_action( '__footer' ); // hook of footer widget and colophon?>
 	</footer>
 <?php 
 wp_footer(); //do not remove, used by the theme and many plugins
